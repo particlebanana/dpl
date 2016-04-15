9f54236fbc15a544f01000402c82e5c3bc9fcac0
Matt Knox <matt.knox@sphero.com>
2016-04-15 14:44:52 -0600

    Let's see how tr does

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -78,7 +78,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | sed 's/\e\[1G//g'"
+        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -cd '[:print:]'"
           error 'Deploying application failed.'
         end
       end
