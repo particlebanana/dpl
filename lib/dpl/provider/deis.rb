0f7ac7fd0e40404f7878084fb8c0f6958e24e972
Matt Knox <matt.knox@sphero.com>
2016-04-15 12:25:43 -0600

    Sed out the remote: prefix

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -78,7 +78,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1"
+        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | sed 's/remote: //g'"
           error 'Deploying application failed.'
         end
       end
