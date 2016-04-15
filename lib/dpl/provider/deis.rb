17634be3e65f453eaa76559342a371111f48452e
Matt Knox <matt.knox@sphero.com>
2016-04-15 14:47:38 -0600

    Keep newlines as well

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -78,7 +78,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -cd '[:print:]'"
+        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -cd '[:print:]\n'"
           error 'Deploying application failed.'
         end
       end
