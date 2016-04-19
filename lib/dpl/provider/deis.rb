8ad0bf7f27a4ce26f7513b68fd6d8c5514bd64d3
Matt Knox <matt.knox@sphero.com>
2016-04-19 14:48:40 -0600

    Let's try something simpler

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -78,7 +78,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -cd '[:print:]\n' | sed -e 's|remote: \\[1G||' -e 's|\\[K||' -e 's|\\[1G|\\t|'"
+        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -dc '[:alnum:][:space:][:punct:]'"
           error 'Deploying application failed.'
         end
       end
