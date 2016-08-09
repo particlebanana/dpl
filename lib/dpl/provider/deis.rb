30acb08e128d4a9576e8a9672e546dbe9c11e216
Matt Knox <matt.knox@sphero.com>
2016-08-09 10:59:01 -0600

    Original shell needs to exit with the same status

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -96,7 +96,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "bash; git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -dc '[:alnum:][:space:][:punct:]' | sed -E 's/remote: (\\[1G)+//' | sed 's/\\[K$//'; exit ${PIPESTATUS[0]}"
+        unless context.shell "bash; git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -dc '[:alnum:][:space:][:punct:]' | sed -E 's/remote: (\\[1G)+//' | sed 's/\\[K$//'; exit ${PIPESTATUS[0]}; exit $?"
           error 'Deploying application failed.'
         end
       end
